f6fd66520fbcae1e23d58f9812487e9059820f97
Trini <trcreme@gmail.com>
2019-03-16 15:23:29 -0400

    serviceWorker comments updated

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-// const cacheFiles = ['/', '/index.html', '/css/styles.css', '/js/dbhelper.js',
-//                     '/js/main.js', '/js/restaurant_info.js', '/data/restaurants.json',
-//                     '/img/1.jpg', '/img/2.jpg', '/img/3.jpg', '/img/4.jpg',
-//                     '/img/5.jpg', '/img/6.jpg', '/img/7.jpg', '/img/8.jpg',
-//                     '/img/9.jpg', '/img/10.jpg'];
-
+// Adds eventlisterner to serviceWorker to install the cache
   self.addEventListener('install', function(e) {
-    // console.log('Installing Cache.');
+
       e.waitUntil(
           caches.open('v1').then(function(cache) {
-            // console.log('Creating Cache.');
+          //creating cache
               return cache.addAll([
                 './',
                 './index.html',
@@ -31,16 +26,16 @@
           })
       );
   });
-
+// Fetches cache data when network is unavailable
   self.addEventListener('fetch', function(e) {
       e.respondWith(
           caches.match(e.request).then(function(response) {
             if (response) {
-              // console.log('Found', e.request, ' in cache, FETCHING!');
+
               return response;
             }
             else {
-              // console.log('Could not find ', e.request, ' in cache, FETCHING!');
+
               return fetch(e.request)
               .then(function(response) {
                   const clonedResponse = response.clone();
